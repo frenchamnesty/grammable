1f0687a4f14a8f9f5e93d64a97f3ec2eb7af5cc2
Kathleen French <kfrench09@gmail.com>
2017-03-21 19:23:12 -0400

    revert

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,10 +7,5 @@ CarrierWave.configure do |config|
     :aws_secret_access_key  => ENV["AWS_SECRET_KEY"]                         # required
   }
   config.fog_directory  = ENV["AWS_BUCKET"]                     # required
-
-  bucket.files.create {
-    key: 'user_1_files/',
-    body: nil
-  }
-
+  
 end
